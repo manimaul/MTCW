636c0b2240e73130089604e2a6eb1ae9e870d3bd
Will Kamp <manimaul@gmail.com>
2012-01-01 22:13:19 -0800

    cleanup

--- BsbOutlines.py
+++ BsbOutlines.py
@@ -41,32 +41,3 @@ class BsbOutlines():
     
     def getzooms(self, key):
         return self.data[key][5]
-        
-    
-if __name__== "__main__":
-    from Sqlite3 import DataStore, createdb
-    import NoaaXmlParser as nxl
-    import os.path
-    import time
-    now = str(int(time.time()))
-    
-    for key in nxl.descriptions.keys():
-        if len(key) is 9:
-            dbf = '/home/will/charts/gemfs/%s.s3db' %(key)
-            if os.path.isfile(dbf):
-                print "removing: " + dbf
-                os.remove(dbf)
-            if not os.path.isfile(dbf):
-                print "creating: " + dbf
-                createdb(dbf)
-            print dbf.rstrip(".s3db")+".gemf"
-            ds = DataStore(os.path.abspath(dbf))
-            #date, initcenter, initzoom, name, description
-            ds.addmain(now, 0, 0, key, nxl.descriptions[key])
-            dir = "/home/will/charts/BSB_ROOT"
-            filter = nxl.NoaaXmlParser(key).getKapFiles()
-            bo = BsbOutlines(dir, filter)
-            for key in bo.getkeys():
-                #print key
-                ds.addchart(key, bo.getname(key), bo.getupdated(key), bo.getscale(key), bo.getoutline(key), bo.getdepthunits(key), bo.getzooms(key))
-            ds.Close()
